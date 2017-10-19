c3118771fed9d49d21e0cc218a186ead2ab3c4a0
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-10-19 02:13:34 +0000

    trying to resolve return from api

--- js/main.js
+++ js/main.js
@@ -3,5 +3,19 @@
 //begin
     $("#searchButton").on("click", function(){
         var searchInput = $("searchInput").val();
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchInput + "&format=json&callback=?";
         
-    })
+        $.ajax({
+            url: url,
+            type: 'GET',
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: "json",
+            success: function(data, status, jqXHR) {
+                $("#returnSearch").html();
+                for(var i=0; i<data[1].length; i++){
+                    $("#returnSearch").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div>")
+                }
+            }
+        });
+    });
