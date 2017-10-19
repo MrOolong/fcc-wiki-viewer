23adbad393c70a69f7e5d3c9eb56b1550fe3b0a6
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-10-19 03:36:07 +0000

    functional API call for open search terms, unable to integrate category info into results

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 //begin
     $("#searchButton").on("click", function(){
-        var searchInput = $("searchInput").val();
+        var searchInput = $("#searchInput").val();
         var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchInput + "&format=json&callback=?";
         
         $.ajax({
@@ -12,6 +12,7 @@
             async: false,
             dataType: "json",
             success: function(data, status, jqXHR) {
+                console.log(data);
                 $("#returnSearch").html();
                 for(var i=0; i<data[1].length; i++){
                     $("#returnSearch").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div>")
